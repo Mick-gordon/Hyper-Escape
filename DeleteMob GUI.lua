7b8692bce509ccc25bacc0c7eab05f6f2fbb823c
Mick Gordon <78802703+Mick-gordon@users.noreply.github.com>
2024-05-20 23:17:00 +0100

    Update DeleteMob GUI.lua

--- DeleteMob GUI.lua
+++ DeleteMob GUI.lua
@@ -24,7 +24,7 @@ local library = {
 	function library:CreateWindow(Keybind, Name)
 		local window = { };
 		window.keybind = Keybind or Enum.KeyCode.RightShift;
-		window.name = Name;
+		window.name = Name or "DeleteMob";
 
 		window.ScreenGui = Instance.new("ScreenGui");
 		window.ScreenGui.Parent = (CoreGui or StarterGUI);
